44f1ed3979e1a650b634641e1a7092994afcf533
Fallen <habibnaufal621@gmail.com>
2025-07-26 14:29:48 +0700

    peerubahan sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
 import { assets } from '../../assets/assets.js'
-import { Link, useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 function Sidebar() {
     const [isExpanded, setIsExpanded] = React.useState(true)
     const location = useLocation();
 
-    const getLinkClass = (path) => 
-        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 
-        ${location.pathname === path ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'}`;
+
 
 
     return (
@@ -30,51 +28,75 @@ function Sidebar() {
 
                 {/* MENU */}
                 <nav className="mt-4 flex flex-col gap-2 px-4 text-sm">
-                    <Link to="/" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    <NavLink to="/" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.dashboardIcon} alt="Dashboard" className="w-5 h-5" />
                         {isExpanded ? <p>Dashboard</p> : null}
-                    </Link>
-                    <Link to="/users" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    </NavLink>
+                    <NavLink to="/users" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.usersIcon} alt="Users" className="w-5 h-5" />
                         {isExpanded ? <p>Users</p> : null}
-                    </Link>
-                    <Link to="/sets" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    </NavLink>
+                    <NavLink to="/sets" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.setsIcon} alt="Sets" className="w-5 h-5" />
                         {isExpanded ? <p>Sets</p> : null}
-                    </Link>
-                    <Link to="/rarities" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    </NavLink>
+                    <NavLink to="/rarities" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.raritiesIcon} alt="Rarities" className="w-5 h-5" />
                         {isExpanded ? <p>Rarities</p> : null}
-                    </Link>
-                    <Link to="/products" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    </NavLink>
+                    <NavLink to="/products" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.productIcon} alt="Products" className="w-5 h-5" />
                         {isExpanded ? <p>Product</p> : null}
-                    </Link>
-                    <Link to="/address" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    </NavLink>
+                    <NavLink to="/address" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.addressIcon} alt="Address" className="w-5 h-5" />
                         {isExpanded ? <p>Address</p> : null}
-                    </Link>
-                    <Link to="/orders" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    </NavLink>
+                    <NavLink to="/orders" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.orderIcon} alt="Orders" className="w-5 h-5" />
                         {isExpanded ? <p>Orders</p> : null}
-                    </Link>
-                    <Link to="/order-items" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                    </NavLink>
+                    <NavLink to="/order-items" className={({ isActive }) =>
+                        `rounded px-2 py-2 cursor-pointer flex items-center gap-2 ${isActive ? 'bg-blue-700 text-white' : 'hover:bg-neutral-500'
+                        }`
+                    }>
                         <img src={assets.orderItemIcon} alt="OrdersItems" className="w-5 h-5" />
                         {isExpanded ? <p className="">Order Items</p> : null}
-                    </Link>
+                    </NavLink>
                 </nav>
             </div>
 
             {/* BOTTOM */}
             <div className="px-4 py-4 border-t border-neutral-500 text-sm">
-                <Link to="settings" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer mb-2 flex items-center gap-2">
+                <NavLink to="settings" className="hover:bg-neutral-500 rounded px-2 py-2 cursor-pointer mb-2 flex items-center gap-2">
                     <img src={assets.settingsIcon} alt="Settings" className="w-5 h-5" />
                     {isExpanded ? <p>Settings</p> : null}
-                </Link>
-                <Link to="logout" className="hover:bg-red-600 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
+                </NavLink>
+                <NavLink to="logout" className="hover:bg-red-600 rounded px-2 py-2 cursor-pointer flex items-center gap-2">
                     <img src={assets.logoutIcon} alt="Logout" className="w-5 h-5" />
                     {isExpanded ? <p>Logout</p> : null}
-                </Link>
+                </NavLink>
             </div>
         </div>
     )
